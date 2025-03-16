eee941a3931098383cfee20177195e1b0e3aab13
Talin Herold <talinherold@gmail.com>
2025-03-16 21:40:32 +0100

    Updated testing files.

--- tests/TutorialTests/tutorial_2_test.py
+++ tests/TutorialTests/tutorial_2_test.py
@@ -37,6 +37,7 @@ except ValueError:
 
 regions.remove('Pluto')
 e.region_names = regions
+e.region_names[4] = 'Jupiter'
 e.votes[-1,-1] = 11404
 
 
@@ -89,9 +90,80 @@ except bp.InvalidOrderError:
     pass
 if np.any(e.seats != expected_seats):
     raise ValueError('Calculated seats does not match expected value.')
+
+
+###############################################################################
+#                                                                             #
+#   Post-apportionment phase                                                  #
+#                                                                             #
+###############################################################################
+reordered_seats = np.array([[1, 4, 2, 3, 4, 2, 5, 8],
+                            [0, 4, 0, 2, 3, 1, 4, 2],
+                            [0, 3, 1, 3, 2, 0, 3, 3],
+                            [0, 1, 1, 2, 3, 0, 4, 1],
+                            [1, 2, 1, 2, 0, 2, 1, 2],
+                            [0, 1, 1, 1, 2, 1, 3, 2],
+                            [0, 0, 0, 1, 0, 0, 2, 1],
+                            [0, 0, 0, 0, 1, 0, 0, 1],
+                            [0, 0, 0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0, 0]], dtype=int)
 e.reorder(party_order='seats')
 if e.parties != ['Sparrows', 'Waxwings', 'Owls', 'Sparrowhawks', 'Blackbirds', 'Geese', 'Starlings', 'Cuckoos', 'Thrushes', 'Ducks']:
     raise ValueError('Reordered parties are not in the expected order.')
+if np.any(e.seats != reordered_seats):
+    raise ValueError('Reordered seats do not match expected seats.')
+
+old_votes = e.votes.sum(axis=1)[-2:].sum()
+e.reorder(irr_parties='other')
+new_votes = e.votes[-1].sum()
+if old_votes != new_votes:
+    ValueError('The votes of the "other" parties changed unexpectedly after reordering')
+
+e.reorder(party_order='alphabetical', irr_parties='other', other_parties_name='Loosers', other_parties_at_end=False)
+if e.parties != ['Blackbirds', 'Cuckoos', 'Geese', 'Loosers', 'Owls', 'Sparrowhawks', 'Sparrows', 'Starlings', 'Waxwings']:
+    raise ValueError('Reordered parties are not in the expected order.')
+
+e.reorder(party_order='votes', irr_parties='other')
+if e.parties != ['Sparrows', 'Waxwings', 'Owls', 'Sparrowhawks', 'Geese', 'Blackbirds', 'Starlings', 'Cuckoos', 'other']:
+    raise ValueError('Reordered parties are not in the expected order.')
+
+try:
+    e.upper_apportionment(which='parties')
+    raise RuntimeError('Expected InvalidOrderError but received none.')
+except bp.InvalidOrderError:
+    pass
+
 e.reorder(region_order='alphabetical')
-if e.regions != ['Earth', 'Jupyter', 'Mars', 'Mercury', 'Neptune', 'Saturn', 'Uranus', 'Venus']:
+if e.regions != ['Earth', 'Jupiter', 'Mars', 'Mercury', 'Neptune', 'Saturn', 'Uranus', 'Venus']:
     raise ValueError('Reordered regions are not in the expected order.')
+
+custom_order = ['Venus', 'Earth', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Earth', 'Pluto']
+e.reorder(region_order=custom_order)
+if e.regions != ['Venus', 'Earth', 'Jupiter', 'Saturn', 'Uranus', 'Neptune', 'Mars', 'Mercury']:
+    raise ValueError('Reordered regions are not in the expected order.')
+
+custom_order = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
+e.reorder(region_order=custom_order)
+if e.regions != custom_order:
+    raise ValueError('Reordered regions are not in the expected order.')
+
+e.reorder(party_order='alphabetical', irr_parties='other', region_order='votes')
+if e.parties != ['Blackbirds', 'Cuckoos', 'Geese', 'Owls', 'Sparrowhawks', 'Sparrows', 'Starlings', 'Waxwings', 'other']:
+    raise ValueError('Reordered parties do not match the expected order')
+if e.regions != ['Uranus', 'Neptune', 'Venus', 'Jupiter', 'Mars', 'Earth', 'Saturn', 'Mercury']:
+    raise ValueError('Reordered regions do not match the expected order')
+
+e.reorder_parties(party_order='votes', irr_parties='other')
+if e.parties != ['Sparrows', 'Waxwings', 'Owls', 'Sparrowhawks', 'Geese', 'Blackbirds', 'Starlings', 'Cuckoos', 'other']:
+    raise ValueError('Reordered parties do not match the expected order')
+e.reorder_regions(region_order=custom_order)
+if e.regions != custom_order:
+    raise ValueError('Reordered regions do not match the expected order')
+
+votes_before = e.total_votes
+e.reorder(irr_parties='delete')
+votes_after = e.total_votes
+if e.shape != (8, 8):
+    raise ValueError(f'Election object has shape {e.shape}, but expected shape (8, 8).')
+if votes_after != votes_before:
+    raise ValueError(f'Votes before ({votes_before}) do not match votes after ({votes_after}) deletion.')
